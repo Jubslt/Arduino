a574b654aa9e69a818845f86d2a8806fced345eb
Jubslt <julia.torres21@hotmail.com>
2024-03-04 13:54:40 -0300

    adicionando execicio

---
+++ Exercícios/Ex 03.cpp
@@ -0,0 +1,19 @@
+int seg_a = 3;
+int seg_b = 4;
+int seg_c = 5;
+int seg_d = 6;
+int seg_e = 7;
+int seg_f = 8;
+int seg_g = 9;
+int bot = 2;
+short int m = 0;
+
+void setup() {
+  pinMode(seg_a, OUTPUT);
+  pinMode(seg_b, OUTPUT);
+  pinMode(seg_c, OUTPUT);
+  pinMode(seg_d, OUTPUT);
+  pinMode(seg_e, OUTPUT);
+  pinMode(seg_f, OUTPUT);
+  pinMode(seg_g, OUTPUT);
+}
